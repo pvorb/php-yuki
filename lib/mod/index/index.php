c84116b4fc3715740ceaf3a55574e073031b02af
Paul Vorbach <vorbach@genitis.org>
2010-09-30 03:30:04 +0200

    Started development on the indexing module.

---
+++ lib/mod/index/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Indexing module.
+ *
+ * Provides functionality for dynamically creating an index over HTML documents.
+ *
+ * @author Paul Vorbach <vorbach@genitis.org>
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ * @version 0.1.0
+ * @package org.genitis.yuki.mod.index
+ */
+
+// TODO class for creating an index
