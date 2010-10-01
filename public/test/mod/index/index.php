6949d07b0c26dcee78533a245f1f90eb7eae8db2
Paul Vorbach <vorbach@genitis.org>
2010-10-01 14:42:29 +0200

    Added a test for the new index module.

---
+++ public/test/mod/index/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Tests for class index.
+ *
+ * @author Paul Vorbach <vorbach@genitis.org>
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ * @package org.genitis.yuki.mod.index.test
+ */
+?>
+<h1>Tests for class <code>index</code></h1>
+<h2>Contents of folder <code>/public</code></h2>
+<?php
+$index = new index(DIR_PUB, '');
+$index->print_list();
+?>
+<h2>Contents of folder <code>/public</code> matched against a pattern</h2>
+<p>The pattern used is <code>test/mod/[[:alpha:]]+/[[:alpha:]]+</code>.</p>
+<?php
+$index = new index(DIR_PUB, 'test/mod/[[:alpha:]\-]+/[[:alpha:]\-]+');
+$index->print_list();
