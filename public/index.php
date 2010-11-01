72dd7e4dd980ffe0596191b86e2f2247a58fb354
Paul Vorbach <vorbach@genitis.org>
2010-11-01 22:01:47 +0100

    Simplified /public/index.php.

--- public/index.php
+++ public/index.php
@@ -1,82 +1,38 @@
 <?php
 /**
- * This file grabs needed html files.
+ * This file includes requested HTML files.
  *
  * @author Paul Vorbach <vorbach@genitis.org>
  * @license http://opensource.org/licenses/mit-license.php MIT License
- * @version 0.1.2
  * @package org.genitis.yuki
  */
 
+define('DIR_SEP', DIRECTORY_SEPARATOR);
+define('DIR_PUB', dirname(__FILE__).DIR_SEP);
+
 // Requires conf.php
-require_once '../lib/conf.php';
-// Define public dir
-define('DIR_PUB', dirname(__FILE__));
+require '../files/conf.php';
 
-// Set $_GET['q'] as $path, default is ''.
-$path = '';
-if (isset($_GET['q'])) {
-	$path = $_GET['q'];
-	unset($_GET['q']);
+$url = $_GET['url']; // request
+
+// Check for GET parameter 'file'
+if (isset($_GET['file'])) {
+	// Include file
+	get_file($url);
 }
-
-// Requires functions.php
-require_once '../lib/functions.php';
-
-// Set $content and $index
-if ($path == '') {
-	$content = '.'; // does never exist
-	$index = 'index';
-} elseif (strrpos($path, 'index/') === 0) {
-	// Redirect to a 404 error, no content file has been found
-	redirect(404, ERROR_PAGE_404, $path);
-} else {
-	$index = $path.'index';
-	$content = rtrim($path, '/'); // remove trailing slash
+// Check for GET parameter 'dir'
+elseif (isset($_GET['dir'])) {
+	// Include dir
+	get_dir($url);
 }
+// Check for GET parameter 'err'
+elseif (isset($_GET['err'])) {
+	require_once DIR_LIB.'redirections.php';
 
-if (USES_MULTIPLE_LANGUAGES) {
-	// Set a language
-	$lang = 'en';
-	if (isset($_GET['lang'])) {
-		$lang = $_GET['lang'];
-		unset($_GET['lang']);
-	}
-
-	// Append .$lang to $file_path
-	$content .= '.'.$lang;
-	$index .= '.'.$lang;
-} elseif (isset($_GET['lang'])) {
-	redirect(301, $path);
+	// If a redirection for $url is defined, make a redirect as defined.
+	if (isset($redirections[$url]))
+		redirect(301, $redirections[$url]);
+	// Otherwise redirect to a 404 error.
+	else
+		redirect(404, ERROR_PAGE_404, $url);
 }
-
-// Load all modules.
-load_modules($modules);
-
-// Try to include the file with different file endings with the order that was
-// specified in lib/conf.php in variable $file_ext.
-for ($i = 0; $i < sizeof($file_ext); $i++) {
-	// Append file extension
-	$content_path = $content.$file_ext[$i];
-	$index_path = $index.$file_ext[$i];
-
-
-	// Include content
-	if (file_exists($content_path)) {
-		$is_index = FALSE;
-		include $content_path;
-		exit;
-	} elseif (file_exists($index_path)) {
-		$is_index = TRUE;
-		include $index_path;
-		exit;
-	}
-}
-
-// Redirections
-include '../lib/redirections.php';
-if (isset($redirections[$path]))
-	redirect(301, $redirections[$path]);
-
-// Redirect to a 404 error, no content file has been found
-redirect(404, ERROR_PAGE_404, $path);
