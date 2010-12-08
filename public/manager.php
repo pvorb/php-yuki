7c9d41b0b41603d1c05649548b067cc8f26993a1
pvorb <vorbach@genitis.org>
2010-12-08 23:42:56 +0100

    Exit manager.php, when no $_GET[url] is given.

--- public/manager.php
+++ public/manager.php
@@ -13,6 +13,10 @@ define('DIR_PUB', dirname(__FILE__).DIR_SEP);
 // Requires conf.php
 require '../lib/conf.php';
 
+// Exit if $_GET['url'] is not set
+if (!isset($_GET['url']))
+	exit;
+
 $url = $_GET['url']; // request
 
 // Check for GET parameter 'file'
