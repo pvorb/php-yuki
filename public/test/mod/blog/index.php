c5e4e08869a1a556bb23f1af4dad5e50eb6c8d72
pvorb <vorbach@genitis.org>
2011-01-15 03:35:35 +0100

    Added HTML5 wrapper arround test index file.

--- public/test/mod/blog/index.php
+++ public/test/mod/blog/index.php
@@ -1,4 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>Blog index</title>
+		<style>
+			#title { font-size: 1.5em; border-bottom: 1px solid #000; }
+		</style>
+	</head>
+	<body>
+		<div id="title">Blog index</div>
 <?php
 require dirname(__FILE__).DIR_SEP.'conf.php';
 
 $blog->print_index();
+?>
+	</body>
+</html>
